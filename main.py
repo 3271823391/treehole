0fc70cc2347af02374106b7f2c0b3fbf5118f37e
3271823391 <3271823391@qq.com>
2026-01-23 21:49:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
 from config import HOST, PORT
 from routers import page, customize, chat
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI(title="DeepSeek虚拟树洞（精致版）")
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+print("STATIC DIR EXISTS:", os.path.exists(STATIC_DIR))
+print("STATIC ABS PATH:", STATIC_DIR)
+
+app.mount(
+    "/static",
+    StaticFiles(directory=STATIC_DIR),
+    name="static"
+)
+
+from routers import page, customize, chat
 app.include_router(page.router)
 app.include_router(customize.router)
 app.include_router(chat.router)
